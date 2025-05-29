3836a3c2cf56189ac543e9b1f09e7a01acc758a8
James Brand <jamesbrandecon@gmail.com>
2025-05-29 09:02:50 -0700

    increment version, minor example change

--- examples/testing_debias.jl
+++ examples/testing_debias.jl
@@ -5,15 +5,15 @@ using FRACDemand
 
 Plots.theme(:ggplot2) # Change Plots.jl theme
 
-T = 1000; # number of markets/2
-J1 = 2; # number of products in half of markets
+T = 500; # number of markets/2
+J1 = 4; # number of products in half of markets
 J2 = 4; # number of products in the other half of markets
 
 nboot = 30; # Number of bootstrap iterations. Use a larger number in actual usage
  
 original_estimates = []
 debias_estimates = []
-for i = 1:50
+for i = 1:200
     mean_utility_params = [-1 1];
     random_coefficient_sd = [0.3 0.0; 0.0 0.3];
     df = FRACDemand.sim_logit_vary_J(J1, J2, T, 1, mean_utility_params, random_coefficient_sd, 0.3, with_market_FEs = true);
@@ -49,8 +49,13 @@ histogram([debias_estimates[j][name] for j ∈ 1:length(debias_estimates)],
     label = "debiased", 
     normalize = :pdf, 
     alpha = 0.5, 
-    xlabel = string("Estimated", param_name))
+    xlabel = string("Estimated ", param_name))
 histogram!([original_estimates[j][name] for j ∈ 1:length(original_estimates)], 
     label = "original", normalize = :pdf, alpha = 0.5)
-# show truth as vline 
+
 vline!([0.3], color = :black, linestyle = :dash, linewidth = 3, label = "truth")
+vline!([median([debias_estimates[j][name] for j ∈ 1:length(debias_estimates)])], color = 1, linestyle = :dash, linewidth = 3, label = "debiased avg")
+vline!([median([original_estimates[j][name] for j ∈ 1:length(original_estimates)])], color = 2, linestyle = :dash, linewidth = 3, label = "original avg")
+plot!(legend = :outerright, 
+    title = string("Distribution of ", param_name, " estimates"),
+    size = (800, 600))
